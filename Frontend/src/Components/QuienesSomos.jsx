f2f90dfc1459d0200dc6061f26c8a3850bf282e9
Luz <luzmavic17@gmail.com>
2026-01-05 19:16:11 -0300

    css

--- Frontend/src/Components/QuienesSomos.jsx
+++ Frontend/src/Components/QuienesSomos.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import logo1 from "../assets/1.png";
+import logo2 from "../assets/2.png";
+import logo3 from "../assets/3.png";
+import logo4 from "../assets/4.png";
+import logo5 from "../assets/5.png";
+import logo6 from "../assets/6.png";
+import logo7 from "../assets/7.png";
+import logo8 from "../assets/8.png";
+import logo9 from "../assets/9.jpeg";
+import logo10 from "../assets/10.jpeg";
+import logo11 from "../assets/11.jpeg";
+import logo12 from "../assets/12.png";
+import logo13 from "../assets/13.jpg";
 import "../CSS/QuienesSomos.css";
-
+import "../CSS/Catalogo.css";
 const QuienesSomos = () => {
   return (
     <>
@@ -27,7 +40,7 @@ const QuienesSomos = () => {
         <div className="titulo-container flex flex-col items-center justify-center mb-10 mt-10">
           <p className="titulo-naranja font-bold font-[Public-Sans]">
             <span className="titulo-naranja-gradiente">
-              Nuestro Catálogo incluye:
+              Marcas que nos acompañan:
             </span>
           </p>
         </div>
@@ -36,30 +49,125 @@ const QuienesSomos = () => {
           <div className="slot">
             <div className="user-card">
               <span className="user-title">
-                Accesorios galvanizados de 1/4 a 4"
+                <img src={logo1} alt="1 Logo" className="marca-logo" />
               </span>
-            </div>
-          </div>
-
-          <div className="slot">
-            <div className="user-card">
-              <span className="user-title">Accesorios de H° negro</span>
-            </div>
-          </div>
-
-          <div className="slot">
-            <div className="user-card">
-              <span className="user-title">Accesorios serie 2000</span>
             </div>
           </div>
 
           <div className="slot">
             <div className="user-card">
               <span className="user-title">
-                Elementos de seguridad personal
+                <img src={logo2} alt="1 Logo" className="marca-logo" />
               </span>
             </div>
           </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo3} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo4} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo5} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo6} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo7} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo8} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+
+        <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo9} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo10} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo11} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+<div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo12} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+<div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo13} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo11} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+          <div className="slot">
+            <div className="user-card">
+              <span className="user-title">
+                <img src={logo11} alt="1 Logo" className="marca-logo" />
+              </span>
+            </div>
+          </div>
+
+
         </div>
       </section>
     </>
